18f8231b73714284d9d75cedfc8dbf2f0f610912
NarithNR9 <79370506+NarithNR9@users.noreply.github.com>
2022-12-20 15:29:20 +0700

    'PrepareForDeploy'

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8080
@@ -18,10 +19,17 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/users' , userRoutes)
 app.use('/api/tickets' , require('./routes/ticketRoutes'))
 
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
+  app.get('*', (req, res) => res.sendFile(__dirname , '../', 'frontend', 'build', 'index.html'))
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the Support Desk API'})
+  })
+}
 
 app.use(errorHandler)
 
